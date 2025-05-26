8d5c26f517ec4995e14b9067565a0acdb61cdae6
VineethK <kyadasuvineeth@gmail.com>
2025-05-26 21:45:07 +0530

    Added Admin Orders page route

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function AdminOrdersPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-green-800 mb-4">Admin Orders</h1>
+      <p>This is where all orders will be displayed in the future.</p>
+    </div>
+  );
+}
